574e1e5365763130625d94124b8a6edae50c968a
Victor Deryagin <vderyagin@gmail.com>
2014-04-29 08:56:01 +0300

    ZSH script for updating needed env variables.

---
+++ init.zsh
@@ -0,0 +1,11 @@
+path=(
+  "${HOME}/bin"
+  "${HOME}/bin/lib/google-cloud-sdk/bin"
+  "${HOME}/bin/lib/copy/x86_64"
+  "${HOME}/bin/lib/dart-sdk/bin"
+  "${HOME}/bin/lib/odeskteam-3.2.13-1-x86_64/usr/bin"
+)
+
+declare -U path
+
+source "${HOME}/bin/lib/google-cloud-sdk/completion.zsh.inc"
