bd4c8dedfa4a23f9536be845d580b53aa4e74228
Victor Deryagin <vderyagin@gmail.com>
2014-04-29 10:02:22 +0300

    Install Heroku client.
    https://toolbelt.heroku.com/standalone
    https://github.com/heroku/heroku

--- init.zsh
+++ init.zsh
@@ -1,8 +1,9 @@
 path=(
   "${HOME}/bin"
-  "${HOME}/bin/lib/google-cloud-sdk/bin"
   "${HOME}/bin/lib/copy/x86_64"
   "${HOME}/bin/lib/dart-sdk/bin"
+  "${HOME}/bin/lib/google-cloud-sdk/bin"
+  "${HOME}/bin/lib/heroku-client/bin"
   "${HOME}/bin/lib/odeskteam-3.2.13-1-x86_64/usr/bin"
   $path
 )
